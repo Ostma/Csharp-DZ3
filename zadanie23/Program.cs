3a45f398eac63d402ec4b05cfe538ba1c6cc5396
MaxO <ostma@mail.ru>
2022-10-09 19:15:53 +0500

    23

---
+++ zadanie23/Program.cs
@@ -0,0 +1,28 @@
+﻿int ReadData (string line)
+{
+Console.WriteLine(line);
+int number = int.Parse(Console.ReadLine()??"");
+return number;
+}
+
+void PrintData (string prefix, string value)
+{
+    Console.WriteLine(prefix+value);
+}
+
+string LineNum(int numN, int p)
+{
+    string outLine = "1";
+    for(int i=2;i<=numN;i++)
+    {
+        outLine = outLine + "|" + "\t" + Math.Pow(i, p);
+    }
+    return outLine;
+}
+
+
+int numN = ReadData("Введите число: ");
+string lineTop = LineNum(numN, 1);
+string lineDown = LineNum(numN, 3);
+PrintData("", lineTop);
+PrintData("", lineDown);
